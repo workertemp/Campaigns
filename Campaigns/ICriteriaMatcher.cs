b866079ef417f6d6e4a7840148db1f0ae41c94d0
mushfig.agayev <mushfig.agayev@W-ATE-254>
2025-02-11 07:03:47 +0400

    ok

---
+++ Campaigns/ICriteriaMatcher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campaigns
+{
+    #region Criteria Matcher
+    public interface ICriteriaMatcher<T>
+    {
+        Task<CriteriaMatchResult> MatchAsync(T model);
+    }
+
+    public class CriteriaMatcher : ICriteriaMatcher<CarModel>
+    {
+        private readonly ICriteriaSpecification _criteriaSpecification; //Get datas
+        private readonly IExpressionParser _expressionParser;
+        private readonly ICriteriaCache _criteriaCache; 
+
+        public CriteriaMatcher(
+            ICriteriaSpecification criteriaSpecification,
+            IExpressionParser expressionParser,
+            ICriteriaCache criteriaCache)
+        {
+            _criteriaSpecification = criteriaSpecification;
+            _expressionParser = expressionParser;
+            _criteriaCache = criteriaCache;
+        }
+
+        public async Task<CriteriaMatchResult> MatchAsync(CarModel model)
+        {
+            var result = new CriteriaMatchResult { Model = model };
+            var now = DateTime.UtcNow;
+            var campaigns = await _criteriaCache.GetOrLoadAsync(() => _criteriaSpecification.GetCriteriaAsync());
+            var activeCampaigns = campaigns.Where(c => c.IsActive && c.ValidFrom <= now && c.ValidTo >= now);
+
+            foreach (var campaign in activeCampaigns)
+            {
+                var expression = _expressionParser.ParseExpression<CarModel>(campaign.ExpressionString);
+                var isMatch = expression.Compile()(model);
+                if (isMatch)
+                {
+                    result.MatchedCampaigns.Add(campaign);
+                }
+            }
+
+            result.IsMatch = result.MatchedCampaigns.Any();
+            return result;
+        }
+    }
+    #endregion
+}
---
+++ Campaigns/IExpressionParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campaigns
+{
+    public interface IExpressionParser
+    {
+        Expression<Func<T, bool>> ParseExpression<T>(string expressionString);
+    }
+
+    public class CarExpressionParser : IExpressionParser
+    {
+        public Expression<Func<M, bool>> ParseExpression<M>(string expressionString)
+        {
+            if (string.IsNullOrWhiteSpace(expressionString))
+            {
+                throw new ExpressionParsingException("Expression string cannot be empty", null);
+            }
+
+            return DynamicExpressionParser.ParseLambda<M, bool>(
+                new ParsingConfig { ResolveTypesBySimpleName = true },
+                false,
+                expressionString);
+        }
+    }
+}
